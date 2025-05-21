3a6688b900b3b6750cb818b8b3474402a6325f20
ParasRana123 <parasrana579@gmail.com>
2025-05-21 18:30:27 +0530

    Made an endpoint to extract URL and made a function to generate random id

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+// Gives a random id for deployment
+const MAX_LENGTH = 5;
+
+export function generate() {
+    let ans = "";
+    const subset = "123456789qwertyuiopasdfghjklzxcvbnm"
+    for(let i = 0 ; i < MAX_LENGTH ; i++) {
+        ans += subset[Math.floor(Math.random() * subset.length)];
+    }
+    return ans;
+}
