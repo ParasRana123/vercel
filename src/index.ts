530e313f9e83f362fc21cda843646ef2136464be
ParasRana123 <parasrana579@gmail.com>
2025-05-21 21:48:21 +0530

    extracted all the files from repo URL and put it in R2

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import simpleGit from "simple-git";
 import { generate } from "./utils";
 import path from "path";
 import { getAllFiles } from "./file";
+import { uploadFile } from "./aws"
 
 const app = express();
 
@@ -17,6 +18,11 @@ app.post("/deploy" , async (req , res) => {
     await simpleGit().clone(repoUrl , path.join(__dirname , `output/${id}`));
 
     const files = getAllFiles(path.join(__dirname , `output/${id}`));
+
+    files.forEach(async file => {
+        await uploadFile(file.slice(__dirname.length + 1) , file);
+    });
+
     console.log(files);
     // Putting this to s3
     res.json({
