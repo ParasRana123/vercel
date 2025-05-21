197f5653d41518fe803d5bec4ae24aa30a061594
ParasRana123 <parasrana579@gmail.com>
2025-05-21 20:29:07 +0530

    Added account creditionals for the R2

--- src/aws.ts
+++ src/aws.ts
@@ -1,5 +1,14 @@
 // Uploading the files to the R2 Object Store on Cloud flare
+import { S3 } from "aws-sdk";
+import fs from "fs";
+require("dotenv").config();
+
+const s3 = new S3({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    endpoint: process.env.S3_ENDPOINT
+})
 
 export const uploadFile = (fileName: string , localFilePath: string) => {
-    
+    console.log("Called");
 }
