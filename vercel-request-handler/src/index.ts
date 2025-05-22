d83efdb4575bfbf6636b34816d862c08c840f7f3
ParasRana123 <parasrana579@gmail.com>
2025-05-22 10:00:50 +0530

    Made an endpoint to log the id of the user

--- vercel-request-handler/src/index.ts
+++ vercel-request-handler/src/index.ts
@@ -2,4 +2,11 @@ import express from "express"
 
 const app = express();
 
+app.get("/*" , (req , res) => {
+    const host = req.hostname;
+    console.log(host);
+    const id = host.split(".")[0];
+    console.log(id);
+})
+
 app.listen(3001);
